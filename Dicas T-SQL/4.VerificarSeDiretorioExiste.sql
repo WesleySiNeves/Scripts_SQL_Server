0f7f2dadc5d6a27da67c3caba3f6fc14ace2c76e
Wesley Neves <56141520+WesleySiNeves@users.noreply.github.com>
2022-02-18 20:47:15 -0300

    alterações na estrutura do projeto

--- Dicas T-SQL/4.VerificarSeDiretorioExiste.sql
+++
@@ -1,7 +0,0 @@
-DECLARE @output INT;
-EXEC @output = XP_CMDSHELL 'DIR "D:\Geral\Logs Recuperados\crea-ba\crea-ba.implanta.net.br_Log-031_Logs.csv" /B',
-                           NO_OUTPUT;
-IF @output = 1
-    PRINT 'File Donot exists';
-ELSE
-    PRINT 'File exists';
